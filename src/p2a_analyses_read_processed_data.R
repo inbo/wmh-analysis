7da442bc79d374872f8ab6ac0f998db998d00f56
stijnvanhoey <stijnvanhoey@gmail.com>
2018-11-05 13:50:41 +0100

    Fix relative path handling

--- src/p2a_analyses_read_processed_data.R
+++ src/p2a_analyses_read_processed_data.R
@@ -2,18 +2,16 @@ library(lubridate);library(circular);library(ggplot2)
 library(maptools);library(lattice);library(fossil)
 library(RColorBrewer)
 
-setwd("C:/Users/wvanste1/Documents/2017 WGK - Marsh Harrier/")
-
 #### STEP 1: GATHER ALL DATA
 #############################
 # gather INBO and WGK data from csv produced previously, containing tracks
 # resampled to hourly intervals
-data <- read.csv("MH-resampled-23092017.csv")
+data <- read.csv("data/MH-resampled-23092017.csv")
 data <- data[,c("dev","name","dt","lat","long","alt","date","mth","yr","season","yday","migr","origin","indday")]
 
 data$dt <- as.POSIXct(strptime(data$dt, format="%Y-%m-%d %H:%M:%S"), tz='UTC')
 
-# extract julian day since 1st january 2000 for tracking data 
+# extract julian day since 1st january 2000 for tracking data
 data$date <- as.Date(data$dt,tz='UTC')
 data$julian <- as.numeric(julian(trunc(data$dt, "days"), origin = as.POSIXct("2000-01-01 00:00:00", tz="UTC")))
 # create unique identifier for each ID/day based on julian day
@@ -37,7 +35,7 @@ data <- rbind(data,swedes)
 
 #### STEP 2: CALCULATE HOURLY & DAILY MOVEMENT STATISTICS
 ##########################################################
-# manually remove some outliers 
+# manually remove some outliers
 touts <- c("2012-03-24 04:09:06","2012-03-24 19:09:07","2008-09-18 19:43:10","2011-09-20 18:46:48","2011-10-01 10:55:04","2011-10-02 10:47:15","2011-10-03 10:39:06","2011-10-04 10:29:29")
 touts <- as.POSIXct(strptime(touts, format="%Y-%m-%d %H:%M:%S"), tz='UTC')
 rem <- which(data$name == "SW_F1" & data$dt %in% touts)
@@ -58,7 +56,7 @@ calcdir <- function(x) pt2pt.direction(longitude=x$long,latitude=x$lat)
 # Order dataframe chronologically per bird
 data<-data[order(data$dev,data$dt),]
 
-# Split df per bird and apply functions 
+# Split df per bird and apply functions
 v1 <- lapply(split(data,data$dev),"calcdist")
 v2 <- lapply(split(data,data$dev),"calcdur")
 v1b <- lapply(split(data,data$dev),"calcdist.b")
@@ -101,8 +99,8 @@ data$sset_R <- sset[,2]
 data$daynight <- ifelse(data$dt > data$srise_R -3600 & data$dt < data$sset_R + 3600,'day','night')
 
 # remove a few nighttime outliers detected based on threshold spd 5 ms
-data <- data[-which(data$name == "Ben" & data$dt == as.POSIXct(strptime("2016-11-20 05:43:06", format="%Y-%m-%d %H:%M:%S"), tz='UTC')),] 
-data <- data[-which(data$name == "Ben" & data$dt == as.POSIXct(strptime("2016-11-22 03:56:25", format="%Y-%m-%d %H:%M:%S"), tz='UTC')),] 
+data <- data[-which(data$name == "Ben" & data$dt == as.POSIXct(strptime("2016-11-20 05:43:06", format="%Y-%m-%d %H:%M:%S"), tz='UTC')),]
+data <- data[-which(data$name == "Ben" & data$dt == as.POSIXct(strptime("2016-11-22 03:56:25", format="%Y-%m-%d %H:%M:%S"), tz='UTC')),]
 
 #### STEP 4: ANNOTATE GEOGRAPHICAL INFORMATION
 ################################################
